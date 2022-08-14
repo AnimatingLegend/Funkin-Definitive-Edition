bc93a79b98832ead6aa633fbdfb9d73387f59705
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-08-14 15:14:10 -0400

    Update FreeplayState.hx
    Updated GF's freeplay color

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -31,7 +31,7 @@ class FreeplayState extends MusicBeatState
 	var intendedScore:Int = 0;
 
 	private var grpSongs:FlxTypedGroup<Alphabet>;
-	private var coolColors = [0xFF9271FD, 0xFF9271FD, 0xFF223344, 0xFF941653, 0xFFFC96D7, 0xFFA0D1FF, 0xFFFF78BF, 0xFFF6B604];
+	private var coolColors = [0xE1790135, 0xFF9271FD, 0xFF223344, 0xFF941653, 0xFFFC96D7, 0xFFA0D1FF, 0xFFFF78BF, 0xFFF6B604];
 
 	private var curPlaying:Bool = false;
 
@@ -43,7 +43,7 @@ class FreeplayState extends MusicBeatState
 
 		for (i in 0...initSonglist.length)
 		{
-			songs.push(new SongMetadata(initSonglist[i], 1, 'gf'));
+			songs.push(new SongMetadata(initSonglist[i], 0, 'gf'));
 		}
 
 		#if desktop
