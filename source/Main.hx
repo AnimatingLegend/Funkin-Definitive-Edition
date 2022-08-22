2c8968017118d49c6303ae181c77e5961021340f
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-08-22 13:50:11 -0400

    Added Caching Menu

--- source/Main.hx
+++ source/Main.hx
@@ -14,7 +14,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = CachingState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 120; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
@@ -59,6 +59,9 @@ class Main extends Sprite
 
 		if(FlxG.save.data.judgementCounter == null)
 			FlxG.save.data.judgementCounter = false;
+
+		if(FlxG.save.data.accuracy == null)
+			FlxG.save.data.accuracy = true;
 	}
 
 	public static function main():Void
@@ -105,7 +108,7 @@ class Main extends Sprite
 		}
 
 		#if !debug
-		initialState = TitleState;
+		initialState = CachingState;
 		#end
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
