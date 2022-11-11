d3572b798eaa692bd2f62748275a592cb11dc8e3
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-11-11 16:24:41 -0500

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -465,7 +465,7 @@ class TitleState extends MusicBeatState
 				switch (i + 1)
 				{
 					case 1:
-						createCoolText(['ninjamuffin99', 'phantomArcade', 'kawaisprite', 'evilsk8er']);
+						createCoolText(['ninjamuffin', 'phantomArcade', 'kawaisprite', 'evilsker']);
 						// credTextShit.visible = true;
 					case 3:
 						addMoreText('present');
