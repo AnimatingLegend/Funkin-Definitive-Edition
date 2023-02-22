590c09f2c16cacc5f65539086aa146383880c52b
legendlol <83415030+LegendLOL@users.noreply.github.com>
2023-02-22 10:15:36 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -16,68 +16,75 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	#if web
-	var framerate:Int = 60; // How many frames per second the game should run at. [WEB]
-	#else
 	var framerate:Int = 120; // How many frames per second the game should run at.
-	#end
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
-	public static function setupSaveData() {
-	// False =  not enabled by default | True = enabled by default
-		if(FlxG.save.data.judgementCounter == null)
+	public static function setupSaveData() 
+	{
+		// False =  not enabled by default | True = enabled by default
+
+		if(FlxG.save.data.judgementCounter == null) {
 			FlxG.save.data.judgementCounter = false;
+		}
 
-		if(FlxG.save.data.downscroll == null)
+		if(FlxG.save.data.downscroll == null) {
 			FlxG.save.data.downscroll = false;
+		}
 
-		if(FlxG.save.data.middlescroll == null)
+		if(FlxG.save.data.middlescroll == null) {
 			FlxG.save.data.middlescroll = false;
+		}
 
-		if(FlxG.save.data.framerateDraw == null)
+		if(FlxG.save.data.framerateDraw == null) {
 			FlxG.save.data.framerateDraw = 120;
+		}
 
-		if(FlxG.save.data.ghostTapping == null)
+		if(FlxG.save.data.ghostTapping == null) {
 			FlxG.save.data.ghostTapping = false;
+		}
 
-		if(FlxG.save.data.explicitContent == null)
+		if(FlxG.save.data.explicitContent == null) {
 			FlxG.save.data.explicitContent = true;
+		}
 
-		if(FlxG.save.data.flashingLights == null)
+		if(FlxG.save.data.flashingLights == null) {
 			FlxG.save.data.flashingLights = true;
+		}
 
-		if(FlxG.save.data.notesplash == null)
+		if(FlxG.save.data.notesplash == null) {
 			FlxG.save.data.notesplash = true;
+		}
 		
-		if(FlxG.save.data.glowStrums == null)
+		if(FlxG.save.data.glowStrums == null) {
 			FlxG.save.data.glowStrums = true;
+		}
 
-		if(FlxG.save.data.accuracy == null)
+		if(FlxG.save.data.accuracy == null) {
 			FlxG.save.data.accuracy = true;
+		}
 
-		if(FlxG.save.data.camhudZoom == null)
+		if(FlxG.save.data.camhudZoom == null) {
 			FlxG.save.data.camhudZoom = true;
+		}
 
-		if(FlxG.save.data.ratingHUD == null)
-			FlxG.save.data.ratingHUD = false;
+		if(FlxG.save.data.ratingHUD == null) {
+			FlxG.save.data.ratingHUD = true;
+		}
 
-		if(FlxG.save.data.scrollSpeed == null)
+		if(FlxG.save.data.scrollSpeed == null) {
 			FlxG.save.data.scrollSpeed = 1;
+		}
 
-		/*if(FlxG.save.data.songBar == null)
-			FlxG.save.data.songBar = false;*/
-
-		if(FlxG.save.data.colors == null)
-			FlxG.save.data.colors = true;
-
-		if(FlxG.save.data.lowData == null)
+		if(FlxG.save.data.lowData == null) {
 			FlxG.save.data.lowData = true;
+		}
 
-		if(FlxG.save.data.fps == null)
+		if(FlxG.save.data.fps == null) {
 			FlxG.save.data.fps = true;
+		}
 	}
 
 	public static function main():Void
@@ -123,18 +130,18 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		// dont mind this code.. [Line 117 - 125]
-		#if desktop
+		#if !cpp
+		framerate = 60;
+		#end
+
+		#if cpp
 		initialState = CachingState;
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#else
-		initialState = TitleState;
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#end
 
-		#if debug
-		initialState = TitleState;
-		#end
-
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(game);
 
 		#if !mobile
 		fpsCounter = new FPSCounter(10, 3, 0xFFFFFF);
@@ -150,10 +157,10 @@ class Main extends Sprite
 		Conductor.offset = FlxG.save.data.notesOffset;
 	}
 
+	var game:FlxGame;
 	var fpsCounter:FPSCounter;
 
-	public function toggleFPS(fpsEnabled:Bool):Void 
-	{
+	public function toggleFPS(fpsEnabled:Bool):Void {
 		fpsCounter.visible = fpsEnabled;
 	}
 }
